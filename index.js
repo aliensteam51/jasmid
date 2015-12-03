f01106f1a56a614b7ec10da14e4ab407e2d9831b
Michiel van den Anker <michiel@team51.nl>
2015-12-03 15:34:10 +0100

    Browserified the code, Standard JS formatted

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports.Audio = require('./audio')
+module.exports.MidiFile = require('./midifile')
+module.exports.Replayer = require('./replayer')
+module.exports.Stream = require('./stream')
+module.exports.Synth = require('./synth')
