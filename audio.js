61d31875961812072c28a9f3dfc90fab52eaba47
Michiel van den Anker <michiel@team51.nl>
2015-12-03 15:33:06 +0100

    Removed Flash fallback

--- audio.js
+++ audio.js
@@ -78,48 +78,9 @@ function AudioPlayer(generator, opts) {
 		}
 
 	} else {
-		// Fall back to creating flash player
-		var c = document.createElement('div');
-		c.innerHTML = '<embed type="application/x-shockwave-flash" id="da-swf" src="da.swf" width="8" height="8" allowScriptAccess="always" style="position: fixed; left:-10px;" />';
-		document.body.appendChild(c);
-		var swf = document.getElementById('da-swf');
-		
-		var minBufferDuration = latency * 1000; /* refill buffer when there are only this many ms remaining */
-		var bufferFillLength = latency * sampleRate;
-		
-		function write(data) {
-			var out = new Array(data.length);
-			for (var i = data.length-1; i != 0; i--) {
-				out[i] = Math.floor(data[i]*32768);
-			}
-			return swf.write(out.join(' '));
-		}
-		
-		function checkBuffer() {
-			if (swf.bufferedDuration() < minBufferDuration) {
-				write(generator.generate(bufferFillLength));
-			};
-			if (!requestStop && !generator.finished) setTimeout(checkBuffer, checkInterval);
-		}
-		
-		function checkReady() {
-			if (swf.write) {
-				checkBuffer();
-			} else {
-				setTimeout(checkReady, 10);
-			}
-		}
-		checkReady();
-		
-		return {
-			'stop': function() {
-				swf.stop();
-				requestStop = true;
-			},
-			'bufferedDuration': function() {
-				return swf.bufferedDuration();
-			},
-			'type': 'Flash Audio'
 		}
 	}
+    // No further fallbacks available
+    console.warn('Unable to setup Jasmid audio')
+
 }
